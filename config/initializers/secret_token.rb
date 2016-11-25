2212285e200957bf3296f5b8032fc8dbbe163731
Ireneusz Chojnacki <irek40@interia.pl>
2016-11-25 13:30:57 +0000

    initial commit and sticky gemset

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkRailscms4::Application.config.secret_key_base = '3c25d50b5a5468160206a5e2f95a632c3d332e5f5024883a2cde6c8b4a0dd837f8f76f9443133b4e84426e8c7f905f9ff03e92580d6b26e82028ef494f4cc717'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SkRailscms4::Application.config.session_store :cookie_store, key: '_sk-railscms4_session'
