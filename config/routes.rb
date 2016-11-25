40a6576e4abc6029192c5fccce3d97c52a1e7670
Ireneusz Chojnacki <irek40@interia.pl>
2016-11-25 22:08:23 +0000

    generate Test controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SkRailscms4::Application.routes.draw do
+  get "test/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
