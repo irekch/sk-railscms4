621caf3a91b9e6d62f8aad56768c60956f768fff
Ireneusz Chojnacki <irek40@interia.pl>
2016-12-03 21:08:36 +0000

    create 'uzytkowniks' table in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161203175517) do
+
+  create_table "uzytkowniks", force: true do |t|
+    t.string   "imie",            limit: 20
+    t.string   "nazwisko",        limit: 60
+    t.string   "uzytkownik",      limit: 25
+    t.string   "email",           limit: 100, default: "", null: false
+    t.string   "telefon",                     default: "", null: false
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "uzytkowniks", ["uzytkownik"], name: "index_uzytkowniks_on_uzytkownik"
 
 end
