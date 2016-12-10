3b01a340f27cf94581ded73767a7ef9c96c65ed6
Ireneusz Chojnacki <irek40@interia.pl>
2016-12-10 22:02:39 +0000

    add "edycja" view to "Kategorie"

--- app/controllers/kategories_controller.rb
+++ app/controllers/kategories_controller.rb
@@ -26,15 +26,29 @@ class KategoriesController < ApplicationController
   end
 
   def edycja
+    @kategoria = Kategorie.find(params[:id])
+    @licznik = Kategorie.count
+  end
+  
+  def aktualizuj
+    @kategoria = Kategorie.find(params[:id])
+    if @kategoria.update_attributes(kategorie_parametry)
+      redirect_to(:action=>'pokaz', :id => @kategoria.id)
+    else
+      @licznik = Kategorie.count
+      render('edycja')
+    end
   end
 
   def usun
     @kategoria = Kategorie.find(params[:id])
   end
+  
   def kasuj
     kategoria = Kategorie.find(params[:id]).destroy
     redirect_to(:action=>'index')
   end
+  
   def kategorie_parametry
     params.require(:kategoria).permit(:nazwa, :pozycja, :widoczna)
   end
