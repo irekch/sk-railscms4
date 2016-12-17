7eca796d1dbf490f9ba1a794ce61c9f23658eb41
Ireneusz Chojnacki <irek40@interia.pl>
2016-12-17 18:02:23 +0000

    add "nowy" view to "Artykul"

--- app/controllers/artykuls_controller.rb
+++ app/controllers/artykuls_controller.rb
@@ -10,6 +10,21 @@ class ArtykulsController < ApplicationController
   end
 
   def nowy
+    @artykuly = Artykul.new({:nazwa => "Wprowadź tytuł artykułu"})
+    @strona = Strona.order('pozycja ASC')
+    @licznik = Artykul.count + 1
+  end
+  
+  def utworz
+    @artykuly = Artykul.new(artykuly_parametry)
+    if @artykuly.save
+      flash[:notice] = "Artykuł został pomyślnie utworzony"
+      redirect_to(:action=>'index')
+    else
+      @licznik = Artykul.count + 1
+      @strona = Strona.order('pozycja ASC')
+      render('nowy')
+    end
   end
 
   def edycja
@@ -17,4 +32,9 @@ class ArtykulsController < ApplicationController
 
   def usun
   end
+  
+  def artykuly_parametry
+    params.require(:artykuly).permit(:nazwa, :pozycja, :widoczny, :created_at, :strona_id, :zdjecie)
+  end
+  
 end
