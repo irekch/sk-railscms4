e3cd3e357dedaf20b90f819d412ba17f5fb4375a
Ireneusz Chojnacki <irek40@interia.pl>
2016-12-17 22:21:56 +0000

    introduce paperclip attachment "zdjecie"

--- app/models/artykul.rb
+++ app/models/artykul.rb
@@ -1,6 +1,21 @@
 class Artykul < ActiveRecord::Base
   belongs_to :strona
 
+  # przekopiowane z paperclip dla Rails 4
+  # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  # zmienione dla naszego programu
+  has_attached_file :zdjecie, :styles => { :medium => "600x600>", :thumb => "200x200>" }
+
+  validates_attachment_content_type :zdjecie, 
+                                    :content_type => /^image\/(png|gif|jpeg)/,
+                                    :message => '--- akceptuję tylkopliki GIF JPG i PNG ---'
+
+  validates_attachment_size :zdjecie, 
+                                    :in => 0..150.kilobytes,
+                                    :message => '--- za duży plik max rozmiar to 150kB---'
+
   scope :widoczny, lambda{where(:widoczny => true)}
   scope :niewidoczny, lambda{where(:widoczny => false)}
   scope :sortuj, lambda{order("artykuls.pozycja ASC")}
