1e92b380f85877cad445a3626dc9b5ab0fc62b01
Ireneusz Chojnacki <irek40@interia.pl>
2017-01-06 16:00:04 +0000

    bugfix in spelling

--- app/models/uzytkownik.rb
+++ app/models/uzytkownik.rb
@@ -1,3 +1,24 @@
 class Uzytkownik < ActiveRecord::Base
   has_secure_password
+  
+	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  
+  validates :imie,
+            :presence => true
+            
+  validates :nazwisko,
+            :presence => true
+            
+  validates :uzytkownik,
+            :length => {:within => 4..25, :message => "nazwa użytkownika musi się zawierać między 4 a 25 znaków"},
+            :uniqueness => true
+
+  validates :email,
+            :presence => true,
+            :length => {:maximum => 100},
+            :format => EMAIL_REGEX,
+            :confirmation => true
+            
+  scope :sortuj, lambda{order("nazwisko ASC, imie ASC")}
+
 end
