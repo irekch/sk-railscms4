36021b99362aafb39c323b31cd86ba97366a0cbf
Ireneusz Chojnacki <irek40@interia.pl>
2017-01-07 11:19:14 +0000

    add "nowy" view to "Uzytkownik"

--- app/controllers/uzytkowniks_controller.rb
+++ app/controllers/uzytkowniks_controller.rb
@@ -9,6 +9,18 @@ class UzytkowniksController < ApplicationController
   end
 
   def nowy
+    @uzytkownik = Uzytkownik.new
+  end
+
+  def utworz
+    @uzytkownik = Uzytkownik.new(uzytkownik_parametry)
+    if @uzytkownik.save
+      flash[:notice] = "Użytkownik został pomyślnie dodany"
+      redirect_to(:action=>'index')
+    else
+      @licznik = Uzytkownik.count + 1
+      render('nowy')
+    end
   end
 
   def edycja
@@ -16,4 +28,11 @@ class UzytkowniksController < ApplicationController
 
   def usun
   end
+  
+  private
+  
+  def uzytkownik_parametry
+    params.require(:uzytkownik).permit(:imie, :nazwisko, :uzytkownik, :email, :password, :password_confirmation)
+  end
+  
 end
--- app/models/uzytkownik.rb
+++ app/models/uzytkownik.rb
@@ -2,21 +2,21 @@ class Uzytkownik < ActiveRecord::Base
   has_secure_password
   
 	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
-  
+
   validates :imie,
-            :presence => true
+            :presence => {:in => true, :message => ":pole nie może być puste"}
             
   validates :nazwisko,
-            :presence => true
+            :presence => {:in => true, :message => ":pole nie może być puste"}
             
   validates :uzytkownik,
-            :length => {:within => 4..25, :message => "nazwa użytkownika musi się zawierać między 4 a 25 znaków"},
+            :length => {:within => 4..25, :message => ":nazwa użytkownika musi się zawierać między 4 a 25 znaków"},
             :uniqueness => true
 
   validates :email,
             :presence => true,
             :length => {:maximum => 100},
-            :format => EMAIL_REGEX,
+            :format => {:with => EMAIL_REGEX, :message => ":niepoprawny format"},
             :confirmation => true
             
   scope :sortuj, lambda{order("nazwisko ASC, imie ASC")}
