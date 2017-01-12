eb47aa6ec23da7f3078658a6a0405f250baa2527
Ireneusz Chojnacki <irek40@interia.pl>
2017-01-12 16:25:21 +0000

    add galerie_id param for "Zdjecies" nawigation"

--- app/controllers/zdjecies_controller.rb
+++ app/controllers/zdjecies_controller.rb
@@ -3,13 +3,14 @@ class ZdjeciesController < ApplicationController
   layout 'admin'
   
   before_action :sprawdz_logowanie
+  before_action :szukaj_galerie
   
   def index
-    @zdjecia = Zdjecie.sortuj
+    @zdjecia = @galerie.zdjecies.sortuj
   end
 
   def nowe
-    @zdjecia = Zdjecie.new({:nazwa => "Wprowadź tytuł zdjęcia"})
+    @zdjecia = Zdjecie.new({:galerie_id => @galerie.id, :nazwa => "Wprowadź tytuł zdjęcia"})
     @licznik = Zdjecie.count + 1
     @galeria = Galerie.order('pozycja ASC')
   end
@@ -18,7 +19,7 @@ class ZdjeciesController < ApplicationController
     @zdjecia = Zdjecie.new(zdjecia_parametry)
     if @zdjecia.save
       flash[:notice] = "Zdjęcie zostało pomyślnie zapisane"
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :galerie_id => @galerie.id)
     else
       @licznik = Zdjecie.count + 1
       @galeria = Galerie.order('pozycja ASC')
@@ -40,7 +41,7 @@ class ZdjeciesController < ApplicationController
     @zdjecia = Zdjecie.find(params[:id])
     if @zdjecia.update_attributes(zdjecia_parametry)
       flash[:notice] = "Zdjęcie zostało zmodyfikowane"
-      redirect_to(:action => 'pokaz', :id => @zdjecia.id)
+      redirect_to(:action => 'pokaz', :id => @zdjecia.id, :galerie_id => @galerie.id)
     else
       @licznik = Zdjecie.count
       @galeria = Galerie.order('pozycja ASC')
@@ -55,13 +56,19 @@ class ZdjeciesController < ApplicationController
   def kasuj
     zdjecia = Zdjecie.find(params[:id]).destroy
     flash[:notice] = "Zdjęcie '#{zdjecia.nazwa}' zostało skasowane"
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :galerie_id => @galerie.id)
   end
 
 private
   
   def zdjecia_parametry
     params.require(:zdjecia).permit(:galerie_id, :nazwa, :pozycja, :widoczne, :created_at, :opis, :zdjecie)
+  end
+
+  def szukaj_galerie
+    if params[:galerie_id]
+      @galerie = Galerie.find(params[:galerie_id])
+    end
   end
   
 end
